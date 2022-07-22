0228d69d472a4e3e0a84bb8da1fc926ca622ca0b
Stuart Roeszler <stuart@roeszler.com>
2022-07-22 07:59:41 +0000

    add red part of test_alert_behaviour function

--- test_students.py
+++ test_students.py
@@ -3,9 +3,18 @@ from student import Student
 
 
 class TestStudent(unittest.TestCase):
+
     def test_full_name(self):
         student = Student("John", "Doe")
         self.assertEqual(student.full_name, "John Doe")
+    
+
+    def test_alert_behaviour(self):
+        student = Student("John", "Doe")
+        student.alert_behaviour()
+
+        self.assertTrue(student.attention_list)
+
 
 if __name__ == '__main__':
     """
